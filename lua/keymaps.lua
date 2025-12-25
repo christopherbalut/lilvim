1e409c4e978ac471da0174f5d69907233bb33045
christopherbalut <chris.p.balut@gmail.com>
2025-12-25 00:46:28 -0500

    harpoon keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -52,15 +52,15 @@ vim.keymap.set("n", "<C-h>", function()
 	harpoon:list():select(1)
 end, { desc = "Harpoon file 1" })
 
-vim.keymap.set("n", "<C-t>", function()
+vim.keymap.set("n", "<C-j>", function()
 	harpoon:list():select(2)
 end, { desc = "Harpoon file 2" })
 
-vim.keymap.set("n", "<C-n>", function()
+vim.keymap.set("n", "<C-k>", function()
 	harpoon:list():select(3)
 end, { desc = "Harpoon file 3" })
 
-vim.keymap.set("n", "<C-s>", function()
+vim.keymap.set("n", "<C-l>", function()
 	harpoon:list():select(4)
 end, { desc = "Harpoon file 4" })
 
