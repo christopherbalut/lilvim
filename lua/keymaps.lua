6a71e95fc7906d859bc947aa468ed7156586cb5a
christopherbalut <chris.p.balut@gmail.com>
2026-01-07 22:11:32 -0500

    dap work & none-ls removal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,9 +26,6 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" }) -- desc = LS
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP definition" }) -- desc = LSP definition
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP refactoring suggestions" }) -- desc = "Lsp refactoring suggestions"
 
--- None-ls
---vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "formatting everyting in buffer using none-ls" })
-
 -- Vim-fugitive
 vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Git status" })
 
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,64 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		dapui.setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+		}
+
+		dap.configurations.c = {
+			{
+				name = "Launch (gdb)",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtBeginningOfMainSubprogram = false,
+			},
+			{
+				name = "Attach to process (gdb)",
+				type = "gdb",
+				request = "attach",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				pid = function()
+					return require("dap.utils").pick_process()
+				end,
+				cwd = "${workspaceFolder}",
+			},
+		}
+
+		dap.configurations.cpp = dap.configurations.c
+
+		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+		vim.keymap.set("n", "C-c", function()
+			require("dap").continue()
+		end)
+	end,
+}
