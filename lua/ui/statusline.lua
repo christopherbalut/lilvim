cc79a65d489f4e03dead165c4be776a413d08ac2
christopherbalut <chris.p.balut@gmail.com>
2026-03-29 21:43:43 -0400

    harpoon to stl and fugitive addons

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -1,18 +1,54 @@
 local M = {}
 
-function M.setup()
-	local statusline = {
-		" %t", -- filename tail
-		"%r", -- [RO] if readonly
-		"%m", -- + if modified
-		"%=", -- split left/right
-		"%{&filetype}", -- filetype
-		" %2p%%", -- percent through file
-		" %3l:%-2c:%L ", -- line:col:totalline
-	}
+function M.harpoon_status()
+    local ok, harpoon = pcall(require, "harpoon")
+    if not ok then
+        return ""
+    end
 
-	vim.o.statusline = table.concat(statusline, "")
-	vim.o.laststatus = 3 -- optional: always show statusline
+    local list = harpoon:list()
+    if not list or not list.items then
+        return " H:N"
+    end
+
+    local current = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p")
+    local total = 0
+    local current_idx = nil
+
+    for i, item in ipairs(list.items) do
+        if item and item.value and item.value ~= "" then
+            total = total + 1
+            if vim.fn.fnamemodify(item.value, ":p") == current then
+                current_idx = i
+            end
+        end
+    end
+
+    if total == 0 then
+        return " H:N"
+    end
+
+    if current_idx then
+        return string.format(" H:%d/%d", current_idx, total)
+    end
+
+    return string.format(" H:-/%d", total)
+end
+
+function M.setup()
+    local statusline = {
+        " %t",
+        "%r",
+        "%m",
+        "%{v:lua.require'ui.statusline'.harpoon_status()}",
+        "%=",
+        "%{&filetype}",
+        " %2p%%",
+        " %3l:%-2c:%L ",
+    }
+
+    vim.o.statusline = table.concat(statusline, "")
+    vim.o.laststatus = 3
 end
 
 return M
