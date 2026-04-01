7653918b560175f8bab89cf8ecbf07d42280ef65
christopherbalut <chris.p.balut@gmail.com>
2026-03-31 21:00:46 -0400

    cleaned up some stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,13 +2,13 @@ require("telescope").setup({})
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" }) -- desc = "Telescope find files"
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" }) -- desc = "Telescope live grep"
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" }) -- desc = "Telescope buffers"
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" }) -- desc = "Telescope help tags"
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find git files" })
 vim.keymap.set("n", "<leader>fs", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, {
-	desc = "Find string",
+    desc = "Find string",
 })
