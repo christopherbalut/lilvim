6c825cd47e94f328c4fc099d4b779ff8b857459b
christopherbalut <chris.p.balut@gmail.com>
2026-04-15 22:20:07 -0400

    fixed old lazy files

--- init.lua
+++ init.lua
@@ -17,6 +17,7 @@ vim.pack.add({
     { src = "https://github.com/rafamadriz/friendly-snippets" },
     { src = "https://github.com/stevearc/conform.nvim" },
     "https://github.com/windwp/nvim-autopairs",
+
 })
 
 require("plugins.treesitter")
